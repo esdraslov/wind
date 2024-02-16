9a423b9706db660f611ec3248e2fc0ce3140270f
esdraslov <edvan.marcelino56@gmail.com>
2024-02-16 14:30:19 -0300

    parameters missing errors added

--- npm/wind.js
+++ npm/wind.js
@@ -1,5 +1,11 @@
-let parameters = process.argv
+let parameters = process.argv.slice(2);
 
 // temporally
 //                   start in 2 (why?)
 //console.log(parameters[2])
+
+if (parameters[0] === undefined) {
+    throw new Error('Missing parameter: mode/input file');
+} else if (parameters[1] === undefined && (parameters[0].includes('-') && parameters[0] !== "--open")) {
+    throw new Error('Missing parameter: input file');
+}
