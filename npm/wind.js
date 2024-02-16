73adb88b87581001b73fcde4028b0f0af2c67807
esdraslov <edvan.marcelino56@gmail.com>
2024-02-16 18:16:27 -0300

    modes

--- npm/wind.js
+++ npm/wind.js
@@ -1,3 +1,5 @@
+let { exec } = require("child_process")
+
 let parameters = process.argv.slice(2);
 
 // temporally
@@ -14,4 +16,19 @@ const mode = parameters[0].includes("-") ? parameters[0] : null;
 
 const inputFile = parameters[0].includes("-") ? parameters[1] : parameters[0];
 
-
+if (mode === "--open") {
+    exec(`start http://localhost:8000`, (error, stdout, stderr) => {
+        if (error || stderr) {
+            throw new Error(`exec error: ${error ? error : stderr}`);
+        } else {
+            console.log(`opened\n${stdout}`);
+        }
+    });
+} else if (!mode) {
+    // after
+} else if (mode === "--server") {
+    // after
+} else if (!mode && parameters[2] === "--wvm") {
+    // java
+    // after
+}
